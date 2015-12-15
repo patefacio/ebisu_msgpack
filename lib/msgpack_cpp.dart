a492e32cbdfe965801f350c8492adf2e89f1813c
Daniel Davidson <dbdavidson@yahoo.com>
2015-12-14 20:51:58 -0600

    set namespace on header

--- lib/msgpack_cpp.dart
+++ lib/msgpack_cpp.dart
@@ -22,36 +22,52 @@ class PodMsgpack {
   // custom <class PodMsgpack>
 
   PodMsgpack(this._podPackage, [this._namespace]) {
+    _podCppMapper = new PodCppMapper(podPackage);
+    _header = _podCppMapper.header;
     if (_namespace == null) {
       _namespace = new Namespace(podPackage.name.path);
     }
-    _podCppMapper = new PodCppMapper(podPackage);
-    _header = _podCppMapper.header;
+    _header.namespace = namespace;
     _addSerializerSupport();
   }
 
   _addSerializerSupport() {
     _header.includes.add('msgpack.hpp');
     for (Class cls in _header.classes) {
-      print('PodObjects => ${podPackage.podObjects.map((po) => po.name)}');
       final podObject = podPackage.getType(cls.id.snake);
 
-      cls.getCodeBlock(clsPublic).snippets.add(
-          'MSGPACK_DEFINE(${cls.members.map((m) => m.vname).join(", ")});\n');
       cls
+        ..getCodeBlock(clsPublic).snippets.add(
+          'MSGPACK_DEFINE(${cls.members.map((m) => m.vname).join(", ")});\n')
         ..addFullMemberCtor()
         ..defaultCtor.usesDefault = true;
     }
 
     final podEnums = podPackage.podEnums;
-    _header.getCodeBlock(fcbEndNamespace).snippets.add(brCompact(podEnums
-        .map((pe) => 'MSGPACK_ADD_ENUM(${defaultNamer.nameEnum(pe.id)});')));
+    _header.getCodeBlock(fcbPostNamespace).snippets.add(brCompact(podEnums
+            .map((pe) => 'MSGPACK_ADD_ENUM(${namespace.qualify(defaultNamer.nameEnum(pe.id))});')));
 
     final hasDate = podPackage.allTypes.any((var t) => t.typeName == 'date');
 
+    _header.getCodeBlock(fcbPostNamespace).snippets.add('''
+template< typename T >
+inline msgpack::sbuffer to_msgpack(T const& t) {
+  msgpack::sbuffer sbuf;
+  msgpack::pack(sbuf, t);
+  return sbuf;
+}
+
+template < typename T >
+inline void from_msgpack(msgpack::sbuffer &sbuf, T &t) {
+  msgpack::unpacked msg;
+  msgpack::unpack(msg, sbuf.data(), sbuf.size());
+  msgpack::object obj = msg.get();
+  obj.convert(&t);
+}
+''');
+
     if (hasDate) {
       _header.getCodeBlock(fcbPostNamespace).snippets.add('''
-
 #ifndef __BOOST_DATE_MSGPACK_SERIALIZER__
 #define __BOOST_DATE_MSGPACK_SERIALIZER__
 namespace msgpack {
