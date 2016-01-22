7e25715ac795378ea0dd1633fc592138c8f1c11d
Daniel Davidson <dbdavidson@yahoo.com>
2016-01-22 05:41:54 -0600

    include start of a test

--- test/test_msgpack_cpp.dart
+++ test/test_msgpack_cpp.dart
@@ -7,6 +7,8 @@ import 'package:test/test.dart';
 // custom <additional imports>
 
 import 'package:ebisu_msgpack/msgpack_cpp.dart';
+import 'package:ebisu_pod/ebisu_pod.dart';
+import 'package:ebisu_cpp/ebisu_cpp.dart';
 
 // end <additional imports>
 
@@ -21,11 +23,30 @@ main([List<String> args]) {
   Logger.root.level = Level.OFF;
 // custom <main>
 
+
   test('serialization', () {
     final podMsgpack = new PodMsgpack(balanceSheet);
     final header = podMsgpack.header;
     header.setAsRoot();
     print(header.contents);
+  });
+
+  test('arrays and such', () {
+    var tpp = new PodMsgpack(new PodPackage('foo', namedTypes: [
+      object('o', [
+        field('f', Boolean),
+        field('g', array(Boolean)),
+        field('h', array(Int32)),
+         field(
+            'i',
+            object('ii', [
+              field('iix', object('iii', [field('iiix', Int32)]))
+            ])
+               ),
+      ]),
+      object('p', [field('x', 'o'), field('x', array('o')),]),
+    ]));
+    print(clangFormat(tpp.header.contents));
   });
 
 // end <main>
